2179b29035a5471bd6d27f1f70216cbf8c84966e
Dylan Thomas <thomasdylan76@gmail.com>
2020-04-15 13:52:13 -0600

    Client Details Component

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.clientService.getClient(this.id).subscribe(client => {
+      if(client != null) {
+        if(client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      
+      this.client = client;
+    })
   }
 
 }
