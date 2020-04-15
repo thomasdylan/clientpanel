30401abe22d59a735a747de8017e6d46a4684247
Dylan Thomas <thomasdylan76@gmail.com>
2020-04-15 12:28:26 -0600

    Clients & Dashboard Component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD7jqiAS1tJrsXyKmP7cw59r0iNV1z78XI",
+    authDomain: "client-panel-6e5ab.firebaseapp.com",
+    databaseURL: "https://client-panel-6e5ab.firebaseio.com",
+    projectId: "client-panel-6e5ab",
+    storageBucket: "client-panel-6e5ab.appspot.com",
+    messagingSenderId: "806438029634",
+    appId: "1:806438029634:web:08b2e53c3e96a452dba0db"
+  }
 };
 
 /*
